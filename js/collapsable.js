d8fbf661d95ce50c0fde6092c7ed6ca73695c919
Gisele <giselemigisha53@gmail.com>
2024-02-25 20:49:24 +0200

    Added questions and answers faq section

--- js/collapsable.js
+++ js/collapsable.js
@@ -1,7 +1,22 @@
-document.querySelectorAll("#question_trigger").forEach(question => {
+document.querySelectorAll(".qn_trigger").forEach(question => {
     question.addEventListener("click", function () {
         const answerId = this.getAttribute("trigger-id");
         const answer = document.querySelector(`[collabsable-id="${answerId}"]`);
         answer.classList.toggle("hidden");
     });
 });
+
+function toggleChevron(iconElement) {
+    iconElement.classList.toggle('fa-chevron-down');
+    iconElement.classList.toggle('fa-chevron-up');
+
+    const circleIcon = document.getElementsByClassName('circleIcon');
+    circleIcon.classList.toggle('fa-circle-question');
+    circleIcon.classList.toggle('fa-circle-check');
+
+    if (iconElement.classList.contains('fa-chevron-down')) {
+        iconElement.style.color = '#FFFFFF';
+    } else {
+        iconElement.style.color = '#EAFEFF';
+    }
+}
