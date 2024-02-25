c92d9281f48016c45e1034666d10500e36bc9a46
Shyaka Chaste <liochastej@gmail.com>
2024-02-25 12:34:56 -0800

    new commit, faq page is now working well

--- js/collapsable.js
+++ js/collapsable.js
@@ -1,22 +1,30 @@
+function toggleChevron(iconElement) {
+    if (iconElement && iconElement.classList) {
+        iconElement.classList.toggle('fa-chevron-down');
+        iconElement.classList.toggle('fa-chevron-up');
+
+        const circleIcon = iconElement.parentElement.querySelector('.circleIcon');
+
+        if (circleIcon && circleIcon.classList) {
+            circleIcon.classList.toggle('fa-circle-question');
+            circleIcon.classList.toggle('fa-circle-check');
+        }
+
+        iconElement.style.color = iconElement.classList.contains('fa-chevron-down') ? '#000000' : '#000000';
+    } else {
+        console.error("Invalid icon element provided.");
+    }
+}
+
 document.querySelectorAll(".qn_trigger").forEach(question => {
     question.addEventListener("click", function () {
         const answerId = this.getAttribute("trigger-id");
-        const answer = document.querySelector(`[collabsable-id="${answerId}"]`);
+        const answer = document.querySelector(`[collapsible-id="${answerId}"]`);
         answer.classList.toggle("hidden");
     });
 });
 
-function toggleChevron(iconElement) {
-    iconElement.classList.toggle('fa-chevron-down');
-    iconElement.classList.toggle('fa-chevron-up');
-
-    const circleIcon = document.getElementsByClassName('circleIcon');
-    circleIcon.classList.toggle('fa-circle-question');
-    circleIcon.classList.toggle('fa-circle-check');
-
-    if (iconElement.classList.contains('fa-chevron-down')) {
-        iconElement.style.color = '#FFFFFF';
-    } else {
-        iconElement.style.color = '#EAFEFF';
-    }
+function changeBackground(element) {
+    element.style.backgroundColor = "#EAFEFF";
 }
+
